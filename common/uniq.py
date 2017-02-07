650cb3322286d44b73ca99c3ac29d71d9d387da9
a.murashev <a.murashev@corp.mail.ru>
2017-02-07 16:40:19 +0300

    Add trace param for uniq.py

--- common/uniq.py
+++ common/uniq.py
@@ -3,7 +3,10 @@
 
 import sys
 
-fin = open(sys.argv[1]) if len(sys.argv) > 1 else sys.stdin
+fin = sys.stdin
+
+TRACE_NUM_DEFAULT = 100000
+TRACE_NUM = int(sys.argv[1]) if len(sys.argv) > 1 else TRACE_NUM_DEFAULT
 
 res = set()
 
@@ -12,4 +15,4 @@ for i, line in enumerate(fin, 1):
     if not hash_line in res: 
         res.add(hash_line)
         print line,
-    if i % 100000 == 0: sys.stderr.write("%s\n" % i)
+    if TRACE_NUM > 0 and  i % TRACE_NUM == 0: sys.stderr.write("%s\n" % i)
